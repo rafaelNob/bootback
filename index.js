ba7df7e3df19d07cc042140d1be3756b585a5728
Rafael <nobertorafael9@gmail.com>
2019-10-19 21:32:59 -0300

    atualizacao do projeto com bckp do banco

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+const dateFormat = require('dateformat');
 const restify = require('restify');
 const errs = require('restify-errors');
-const cors = require('cors')
 
 const server = restify.createServer({
     name: 'myapp',
     version: '1.0.0'
 });
 
-var knex = require('knex')({
+let knex = require('knex')({
     client: 'mssql',
     connection: {
         host: 'localhost',
         user: 'sa',
         password: '1234',
-        database: 'dblucca'
+        database: 'dbLucca',
+        port: 1433
     }
 });
-server.use(cors());
+
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
@@ -26,67 +27,108 @@ server.listen(3001, function() {
 });
 
 // rotas REST
+
 server.get('/', (req, res, next) => {
     knex('paciente').then((dados) => {
-        res.send(dados);
+      res.send(dados);
     }, next)
+
 });
 
-/* server.get('/r/horario', (req, res, next) => {
-    knex('horarios').then((dados) => {
-        res.send(dados);
-    }, next)
-}); */
+server.get('/especialidade/:id', (req, res, next) => {
+    let { id } = req.params;
+    id = id.replace('-', '/');
+console.log(id);
 
-server.get('/r/horario', (req, res, next) => {
-    knex('horarios').whereBetween('dHoraInicial', ["2019-10-05","2019-12-30"]).then((dados) => {
-        res.send(dados);
-    }, next)  
+    knex('especialidade')
+        .where('cNmEspecialidade', id)
+        .first()
+        .then((dados) => {
+            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+
+        }, next);
+
 });
 
+server.get('/horarios/:id', (req, res, next) => {
 
-server.get('/r/horario/:id', (req, res, next) => {
     const { id } = req.params;
-    console.log(id);
 
-    knex('horarios')
+    knex('HORARIOS')
         .where('nCdHorario', id)
         .first()
         .then((dados) => {
             if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
             res.send(dados);
-        }, next)
-});
-server.put('/u/horario/:id', (req, res, next) => {
-    console.log("AEO");
-    const { id } = req.params;
-    console.log("pegando o id " + id);
-    console.log("body " + req.body);
-    
-    knex('horarios')
-        .where('nCdHorario', id)
-        .update(req.body)
-        .then((dados) => {
-            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
-            res.send('dados atualizados');
-        }, next)
+
+        }, next);
+
 });
 
-server.post('/c/paciente', (req, res, next) => {
+server.get('/r/datas/desabilitadas/:meses', (req, res, next) => {
+    console.log("caiuss");
+    const { meses } = req.params;
+    let data_atual = new Date();
+    let data_futura = new Date();
+    data_futura.addMonths(parseInt(meses));
+
+    console.log(dateFormat(data_atual, 'yyyy-mm-dd'));
+    console.log(dateFormat(data_futura, 'yyyy-mm-dd'));
+
+    knex('HORARIOS').whereBetween('dHoraInicial', [dateFormat(data_atual, 'yyyy-mm-dd'), dateFormat(data_futura, 'yyyy-mm-dd')])
+        .then((dados) => {
+            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+        }, next);
+});
+ 
+server.get('/tipoConsulta/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    knex('TIPO_CONSULTA')
+        .where('nCdTpConsulta', id)
+        .first()
+        .then((dados) => {
+            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+        }, next);
+});
+
+
+
+server.get('/hospital/:id', (req, res, next) => {
+
+    const { id } = req.params;
+
+    knex('hospital')
+        .where('cNmHospital', id)
+        .first()
+        .then((dados) => {
+            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+
+        }, next);
+
+});
+
+
+server.post('/create', (req, res, next) => {
     console.log(req.body);
+
     knex('paciente')
         .insert(req.body)
         .then((dados) => {
             res.send(dados);
-        }, next)
+          }, next)
 });
 
-server.get('/r/paciente/:cpf', (req, res, next) => {
-    const { cpf } = req.params;
-    console.log(cpf);
+server.get('/paciente/:id', (req, res, next) => {
+    const { id } = req.params;
+    console.log(id);
 
     knex('paciente')
-        .where('nCPF', cpf)
+        .where('nCPF', id)
         .first()
         .then((dados) => {
             if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
@@ -94,10 +136,11 @@ server.get('/r/paciente/:cpf', (req, res, next) => {
         }, next)
 });
 
-server.put('/u/paciente/:id', (req, res, next) => {
+server.put('/update/:id', (req, res, next) => {
     const { id } = req.params;
+
     knex('paciente')
-        .where('nCdPaciente', id)
+        .where('id', id)
         .update(req.body)
         .then((dados) => {
             if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
@@ -105,8 +148,10 @@ server.put('/u/paciente/:id', (req, res, next) => {
         }, next)
 });
 
-server.del('/d/paciente/:id', (req, res, next) => {
+server.del('/delete/:id', (req, res, next) => {
+
     const { id } = req.params;
+
     knex('paciente')
         .where('id', id)
         .delete()
@@ -114,4 +159,60 @@ server.del('/d/paciente/:id', (req, res, next) => {
             if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
             res.send('dados excluidos');
         }, next)
+
+});
+
+
+//TRAZER DATA MAROTA
+server.get('/r/horarios/datasdisponiveis/:nCdEspecialidade/:nCdHospital', (req, res, next) => {
+    const { nCdEspecialidade } = req.params;
+    const { nCdHospital } = req.params;
+    console.log(nCdEspecialidade);
+    console.log(nCdHospital);
+    console.log("caiuu");
+    let data_atual = new Date();
+    let data_futura = new Date();
+    data_futura.addMonths(parseInt(3));
+    // const { meses } = req.params;
+    // data_futura.addMonths(parseInt(meses));
+
+    knex.from('HORARIOS').leftJoin('CONSULTA', 'HORARIOS.nCdHorario', 'CONSULTA.nCdHorario')
+    .leftJoin('HOSPITAL_ESPECIALIDADE', 'HORARIOS.nCdHospital', 'HOSPITAL_ESPECIALIDADE.nCdHospital')
+    .whereBetween('HORARIOS.dHoraInicial', [dateFormat(data_atual, 'yyyy-mm-dd'),dateFormat(data_futura, 'yyyy-mm-dd')])
+    .where({'HOSPITAL_ESPECIALIDADE.nCdEspecialidade':nCdEspecialidade, 'HORARIOS.nCdHospital':nCdHospital})
+    .whereNull('CONSULTA.nCdConsulta')
+    .distinct('HORARIOS.cDATAFORMATADA')
+    .then((dados) => {
+            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+        }, next)
+});
+//knex.from('HORARIOS').innerJoin('CONSULTA', 'HORARIOS.nCdHorario', 'CONSULTA.nCdHorario')
+//TRAZER HORA MAROTA
+
+
+server.get('/r/horarios/horasdisponiveis/:nCdEspecialidade/:nCdHospital/:data', (req, res, next) => {
+    console.log('teste');
+    
+    const { nCdEspecialidade } = req.params;
+    const { nCdHospital } = req.params;
+    let { data } = req.params;
+    let sql = `
+        SELECT HORA_INICIAL = (CONVERT(VARCHAR(12),HORARIOS.dHoraInicial,108))
+            FROM HORARIOS WITH(NOLOCK)
+                INNER JOIN MEDICO_ESPECIALIDADE ON MEDICO_ESPECIALIDADE.nCdMedico = HORARIOS.nCdMedico
+            WHERE CONVERT(DATE,HORARIOS.dHoraInicial)  = CONVERT(DATE, ?)       
+                AND MEDICO_ESPECIALIDADE.nCdEspecialidade = ?
+                AND HORARIOS.nCdMedico = ?
+                AND (
+                    EXISTS(SELECT 1 FROM CONSULTA WITH(NOLOCK) WHERE CONSULTA.nCdHorario = HORARIOS.nCdHorario)
+                    OR EXISTS(SELECT 1 FROM EXAME WITH(NOLOCK) WHERE EXAME.nCdHorario = HORARIOS.nCdHorario )
+                )`;
+    knex.raw(sql, [data, nCdEspecialidade, nCdHospital]).then(function(dados) {
+        console.log('testing1');
+        if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+        res.send(dados);
+        console.log('testing2');
+    });
+    console.log('tested');
 });
