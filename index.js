0896a2e53a7b64746cc24a53f512cf281c5c2363
Rafael <nobertorafael9@gmail.com>
2019-10-20 17:31:34 -0300

    atualizacao do projeto

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.listen(3001, function() {
+server.listen(3001, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
@@ -30,7 +30,7 @@ server.listen(3001, function() {
 
 server.get('/', (req, res, next) => {
     knex('paciente').then((dados) => {
-      res.send(dados);
+        res.send(dados);
     }, next)
 
 });
@@ -38,7 +38,7 @@ server.get('/', (req, res, next) => {
 server.get('/especialidade/:id', (req, res, next) => {
     let { id } = req.params;
     id = id.replace('-', '/');
-console.log(id);
+    console.log(id);
 
     knex('especialidade')
         .where('cNmEspecialidade', id)
@@ -82,7 +82,7 @@ server.get('/r/datas/desabilitadas/:meses', (req, res, next) => {
             res.send(dados);
         }, next);
 });
- 
+
 server.get('/tipoConsulta/:id', (req, res, next) => {
     const { id } = req.params;
 
@@ -120,7 +120,7 @@ server.post('/create', (req, res, next) => {
         .insert(req.body)
         .then((dados) => {
             res.send(dados);
-          }, next)
+        }, next)
 });
 
 server.get('/paciente/:id', (req, res, next) => {
@@ -128,7 +128,7 @@ server.get('/paciente/:id', (req, res, next) => {
     console.log(id);
 
     knex('paciente')
-        .where('nCPF', id)
+        .where('cCPF', id)
         .first()
         .then((dados) => {
             if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
@@ -163,56 +163,134 @@ server.del('/delete/:id', (req, res, next) => {
 });
 
 
+
 //TRAZER DATA MAROTA
+
 server.get('/r/horarios/datasdisponiveis/:nCdEspecialidade/:nCdHospital', (req, res, next) => {
     const { nCdEspecialidade } = req.params;
     const { nCdHospital } = req.params;
-    console.log(nCdEspecialidade);
-    console.log(nCdHospital);
-    console.log("caiuu");
     let data_atual = new Date();
     let data_futura = new Date();
     data_futura.addMonths(parseInt(3));
+    console.log("Bateuuu" + data_atual + " --- " + data_futura);
+
+
     // const { meses } = req.params;
     // data_futura.addMonths(parseInt(meses));
+    let sql = `EXEC consultaDatasDisponiveis '${dateFormat(data_atual, 'yyyymmdd')}', '${dateFormat(data_futura, 'yyyymmdd')}', ?, ? `;
 
-    knex.from('HORARIOS').leftJoin('CONSULTA', 'HORARIOS.nCdHorario', 'CONSULTA.nCdHorario')
-    .leftJoin('HOSPITAL_ESPECIALIDADE', 'HORARIOS.nCdHospital', 'HOSPITAL_ESPECIALIDADE.nCdHospital')
-    .whereBetween('HORARIOS.dHoraInicial', [dateFormat(data_atual, 'yyyy-mm-dd'),dateFormat(data_futura, 'yyyy-mm-dd')])
-    .where({'HOSPITAL_ESPECIALIDADE.nCdEspecialidade':nCdEspecialidade, 'HORARIOS.nCdHospital':nCdHospital})
-    .whereNull('CONSULTA.nCdConsulta')
-    .distinct('HORARIOS.cDATAFORMATADA')
-    .then((dados) => {
-            if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
-            res.send(dados);
-        }, next)
+    knex.raw(sql, [nCdHospital, nCdEspecialidade]).then(function (dados) {
+        console.log('testing1');
+        if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+        res.send(dados);
+        console.log('testing2');
+    });
 });
-//knex.from('HORARIOS').innerJoin('CONSULTA', 'HORARIOS.nCdHorario', 'CONSULTA.nCdHorario')
+
 //TRAZER HORA MAROTA
-
-
 server.get('/r/horarios/horasdisponiveis/:nCdEspecialidade/:nCdHospital/:data', (req, res, next) => {
     console.log('teste');
-    
-    const { nCdEspecialidade } = req.params;
-    const { nCdHospital } = req.params;
+
+    let { nCdEspecialidade } = req.params;
+    let { nCdHospital } = req.params;
     let { data } = req.params;
     let sql = `
         SELECT HORA_INICIAL = (CONVERT(VARCHAR(12),HORARIOS.dHoraInicial,108))
-            FROM HORARIOS WITH(NOLOCK)
+        ,HORARIOS.nCdHorario FROM HORARIOS WITH(NOLOCK)
                 INNER JOIN MEDICO_ESPECIALIDADE ON MEDICO_ESPECIALIDADE.nCdMedico = HORARIOS.nCdMedico
             WHERE CONVERT(DATE,HORARIOS.dHoraInicial)  = CONVERT(DATE, ?)       
                 AND MEDICO_ESPECIALIDADE.nCdEspecialidade = ?
                 AND HORARIOS.nCdMedico = ?
                 AND (
-                    EXISTS(SELECT 1 FROM CONSULTA WITH(NOLOCK) WHERE CONSULTA.nCdHorario = HORARIOS.nCdHorario)
-                    OR EXISTS(SELECT 1 FROM EXAME WITH(NOLOCK) WHERE EXAME.nCdHorario = HORARIOS.nCdHorario )
-                )`;
-    knex.raw(sql, [data, nCdEspecialidade, nCdHospital]).then(function(dados) {
+                    not EXISTS(SELECT 1 FROM CONSULTA WITH(NOLOCK) WHERE CONSULTA.nCdHorario = HORARIOS.nCdHorario)
+                    and not EXISTS(SELECT 1 FROM EXAME WITH(NOLOCK) WHERE EXAME.nCdHorario = HORARIOS.nCdHorario )
+                ) ORDER BY HORARIOS.dHoraInicial`;
+    knex.raw(sql, [data, nCdEspecialidade, nCdHospital]).then(function (dados) {
         console.log('testing1');
         if (!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
         res.send(dados);
         console.log('testing2');
     });
     console.log('tested');
+});
+/*  EXEC inserirConsulta ?,?,?
+cod horario, paciente, especialidade
+
+*/
+
+server.get('/CONSULTA', (req, res, next) => {
+    console.log(req.body);
+
+    knex.select().from('CONSULTA').then(function (CONSULTA) {
+        res.send(CONSULTA);
+    })
+});
+/* server.post('/salvarBanco', function (req, res) {
+    let nCdHorario = req.body.nCdHorario;
+    let nCdPaciente = req.body.nCdPaciente;
+    let nCdEspecialidade = req.body.nCdEspecialidade;
+    knex.raw(`exec inserirConsulta ? , ? , ?`, [nCdHorario, nCdPaciente, nCdEspecialidade])
+        .then(function () {
+            knex.select().from('CONSULTA').
+                then(function (CONSULTA) {
+                    res.send(CONSULTA);
+                })
+
+        })
+    }) */
+    server.post('/CONSULTA', function(req, res) {
+        let paramCodConsulta = req.body.nCdConsulta;
+          let paramCodPaciente = req.body.nCdPaciente;
+          let nCdEspecialidade = req.body.nCdPaciente;
+          console.log("Entrou");
+          
+            knex.raw(`exec inserirConsulta ? , ?, ? `, [paramCodConsulta,paramCodPaciente,nCdEspecialidade])
+              .then(function() {
+                  knex.select().from('CONSULTA').
+                    then(function(CONSULTA){
+                      res.send(CONSULTA);
+                  })
+              })
+      });
+    
+  
+
+/*   dHoraInicial: "28/10/2019 23:00"
+data: "20191028"
+especialidade: "Cardiologia"
+hospital: "Morumbi"
+nCdEspecialidade: 31
+nCdHorario: 75057
+nCdHospital: 2
+nCdPaciente: 1
+ultimaData: "28/10/2019"
+ultimaHora: "23:00 */
+
+/* {
+    "nCdConsulta": 5318,
+    "cDecricao": "UTILIZADO PARA TESTES",
+    "nCdHorario": 74486,
+    "nCdPaciente": 1,
+    "nCdTpConsulta": 1,
+    "dHoraInicial": "2019-10-17T01:30:00.000Z",
+    "dHoraFinal": "2019-10-17T02:00:00.000Z",
+    "nCdMedico": 2,
+    "nCdHospital": 2
+}, */
+
+server.get('/consulta2', (req, res, next) => {
+    knex('consulta').then((dados) => {
+        res.send(dados);
+    }, next)
+
+});
+7
+server.put('/consulta2/insert', (req, res, next) => {
+    console.log(req.body);
+
+    knex('consulta')
+        .insert(req.body)
+        .then((dados) => {
+            res.send(dados);
+        }, next)
 });
